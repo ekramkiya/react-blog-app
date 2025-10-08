36a0c1b0eb98d38a9d856bd38bd16e766d1d55b2
ekram_kiya <ekramkiya00@gmail.com>
2025-10-08 14:11:18 +0430

    complete_blog_project

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import AboutUs from "./pages/aboutUs/AboutUs";
 import { Route, Routes } from "react-router-dom";
 import ArticlePage from "./pages/articlesPage/ArticlePage";
 import CreateArticle from "./pages/createArticle/CreateArticle";
+import NotFound from "./pages/notFound/NotFound";
 function App() {
   return (
     <div className="App">
       <Routes>
+        <Route path="*" element={<NotFound />} />
         <Route path="/" element={<Home />} />
         <Route path="about" element={<AboutUs />} />
         <Route path="/article/:id" element={<ArticlePage />} />
---
+++ src/pages/notFound/NotFound.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../../components/navbar/Navbar";
+
+function NotFound() {
+  return (
+    <div>
+        <Navbar title="کیا بلاگ" />
+      <h1>404</h1>
+    </div>
+  );
+}
+
+export default NotFound;
